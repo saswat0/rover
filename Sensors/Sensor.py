f43637f4751b8972b9f18136d4f390a8320275ed
rover_pi1 <pi@rover.cpm>
2020-01-07 14:00:48 +0000

    Fixed sensors.py

--- Sensors/Sensor.py
+++ Sensors/Sensor.py
@@ -4,16 +4,17 @@ class Sensor:
 	def __init__(self):
 		self.SENSOR_DEVICE_ADDRESS = 0x0d
 		self.SENSOR_BYTES = 20
-		self.I2C_MODE = 2
+		self.I2C_MODE = 1
 		self.bus = SMBus(self.I2C_MODE)
 		self.msg = i2c_msg.read(self.SENSOR_DEVICE_ADDRESS, self.SENSOR_BYTES)
-		self.sensorIndex={'uv': [2], 'temp': [4, 5], 'pressure': [8, 9, 10, 11], 'atmtemp': [8, 9, 10, 11], 'hmidity': [8, 9, 10, 11]}
+		self.sensorIndex={'uv': [2], 'temp': [4, 5], 'pressure': [8, 9, 10, 11], 'atmtemp': [8, 9, 10, 11], 'humidity': [8, 9, 10, 11]}
 	def parseMsg(self, msg):
 		return [hex(i) for i in list(msg)]
 
 	def getAllSensorValue(self):
-		self.bus.i2c_rdwr(self.msg)
-		return parseMsg(self.msg)
+		msg = self.msg
+		self.bus.i2c_rdwr(msg)
+		return self.parseMsg(msg)
 
 	def getSensorValue(self, sensor):
 		sensorValues = self.getAllSensorValue()
